d05479e527002e5daf64f843a5411e82d212827c
xschen <xs0040@gmail.com>
2017-05-16 14:28:06 +0800

    Some unit test using mocha

---
+++ src/normal-distribution.js
@@ -0,0 +1,14 @@
+var jsstats = jsstats || {};
+
+(function(jss){
+	jss.NormalDistribution = function(mean, sd){
+		if(!mean) {
+			mean = 0.0;
+		}
+		if(!sd) {
+			sd = 1.0;
+		}
+		this.mean = mean;
+		this.sd = sd;
+	};
+})(jsstats);
---
+++ test/normal-distribution-spec.js
@@ -0,0 +1,16 @@
+var expect    = require("chai").expect;
+var converter = require("../src/normal-distribution");
+
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+});
